60bc01579ce36baa24eec93969a4cc7a25d4e9f2
Marek Lis <external.marek.lis@kuehne-nagel.com>
2020-01-29 18:44:50 +0100

    Changed the default output file name to YYYYMMDD_HHMMSS_##C_##S.pdf where ##C is the number of cards and ##S is the number of symbols per card.

--- py/cards_create.py
+++ py/cards_create.py
@@ -3,16 +3,20 @@ from disk_scanner import *
 from cards_creator import Cards_Creator
 from dobble_generator import Dobble_Generator
 
+symbols_per_card = 8
+
 files = scan_dir_for_files("./lib/img/")
-print("Found %d files." % (len(files)))
+files_num = len(files)
+print("Found %d files." % (files_num))
 
 gen = Dobble_Generator(8, True)
 cards = gen.generate_cards()
 
-now = datetime.utcnow()
-file_name = str(now.year)+"_"+format(now.month, '02d')+"_"+format(now.day, '02d')+"-" + \
-    format(now.hour, '02d')+"_"+format(now.minute, '02d') + \
-    "_"+format(now.second, '02d')
+now = datetime.now()
+file_name = str(now.year)+""+format(now.month, '02d')+""+format(now.day, '02d')+"_" + \
+    format(now.hour, '02d')+""+format(now.minute, '02d') + \
+    ""+format(now.second, '02d')+"_"+format(files_num, '0d') + \
+    "C_"+format(symbols_per_card, '0d')+"S"
 
 cc = Cards_Creator(8, cards, files)
 cc.generate("./output/" + file_name + ".pdf")
